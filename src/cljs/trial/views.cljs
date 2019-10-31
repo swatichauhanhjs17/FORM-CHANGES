1b5099c40def902bea9f8c87e122b69098dff94f
swati <swatichauhanhjs17@gmail.com>
2019-10-31 20:53:45 +0530

    error showing

--- src/cljs/trial/views.cljs
+++ src/cljs/trial/views.cljs
@@ -18,21 +18,40 @@
 
 
 (defn my-identity
-  [final]
+  [final error]
   [sa/FormInput
    {:label     "NAME"
-
+     :error   "error here"
     :input     "text"
     :value     (get @final :identity)
     :on-change #(swap! final assoc :identity (-> % .-target .-value))}])
 
 (defn my-number
-  [final]
+  [final error]
   [sa/FormInput
    {:label     "NUMBER"
     :input     "number"
+    :error     "error 1"
     :value     (get @final :number)
     :on-change #(swap! final assoc :number (-> % .-target .-value))}])
+
+
+(defn my-email
+  [final]
+  [sa/FormInput
+   {:label     "Email"
+    :input     "text"
+    :value     (get @final :email)
+    :on-change #(swap! final assoc :email(-> % .-target .-value))}])
+
+
+(defn today-date
+  [final]
+  [sa/FormInput
+   {:label     "DOB"
+    :input     "date"
+    :value     (get @final :date)
+    :on-change #(swap! final assoc :date(-> % .-target .-value))}])
 
 
 
@@ -40,12 +59,19 @@
   []
   (let [final (r/atom {:identity "  NAME"
                        :country  " Country"
-                       :number "9876543210"})]
+                       :number "9876543210"
+                       :email "abc@gmail.com"
+                       :date "01/12/19"
+                       })
+        error (r/atom {:error1 "ENTER THE NAME HERE"
+                       :error2 "ENTER YOUR COUNTRY HERE"})]
     (fn []
       [sa/Form {}
-       [my-identity final]
-       [my-country final]
+       [my-identity final error]
+       [my-country final error]
        [my-number final]
+       [my-email final]
+       [today-date final]
        [sa/Button {
                    :circular true
                    :on-click #(re-frame/dispatch [:submit @final])} " SUBMIT"]
@@ -64,7 +90,12 @@
    [sa/ItemContent {}
     [sa/ItemHeader {} "YOUR Number :- " (get last-submitted :number)]
     ]
-
+   [sa/ItemContent {}
+    [sa/ItemHeader {} "YOUR Email :- " (get last-submitted :email)]
+    ]
+   [sa/ItemContent {}
+    [sa/ItemHeader {} "Date :- " (get last-submitted :date)]
+    ]
    ])
 
 (defn show-all-values
